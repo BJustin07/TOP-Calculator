788f778b586b56624f2864f135a30ed3d1b37ac3
bjustin07 <baquiranjustin07@gmail.com>
2026-05-24 23:31:51 +0800

    init:layoutV3.

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("test")
+//todo implementation first of add,delete,multiply,divide with error handling and such.
+//add event listeners to button to manipulate calculator screen content.
+//make clear button clear everything from the calculator screen content
+//make delete button only delete/undo the last input of user
