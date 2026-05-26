433ec615093aeac296688c335b5bb819461cc95f
bjustin07 <baquiranjustin07@gmail.com>
2026-05-26 23:19:23 +0800

    feat:basic calculator functions.

--- script.js
+++ script.js
@@ -1,4 +1,98 @@
-console.log("test")
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function multiply(a,b){
+    if(a === 0 || b === 0){
+        return 0;
+    }
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+
+const calculatorScreen = document.querySelector(".calculator-screen");
+const buttons = document.querySelectorAll("button");
+const operators = ["+","-","x","/"]
+
+function setCalculatorScreen(text){
+    calculatorScreen.textContent = "";
+    calculatorScreen.textContent = text;
+}
+
+function updateCalculatorScreen(updatedText){
+    if(operators.includes(updatedText)){
+        calculatorScreen.textContent = calculatorScreen.textContent + " " + updatedText + " ";
+    }else{
+        calculatorScreen.textContent = calculatorScreen.textContent + updatedText;
+    }
+}
+
+function deletePrevCalculatorScreen(currentCalculatorScreenText){
+    console.log(currentCalculatorScreenText);
+    console.log(currentCalculatorScreenText.substring([0, currentCalculatorScreenText.length-2]))
+    if(currentCalculatorScreenText.length === 1){
+        calculatorScreen.textContent = "";
+        return;
+    }
+    if(currentCalculatorScreenText[currentCalculatorScreenText.length-2] === " "){
+        calculatorScreen.textContent = currentCalculatorScreenText.substring(0,currentCalculatorScreenText.length - 2);
+        return;
+    }
+    calculatorScreen.textContent = currentCalculatorScreenText.substring(0,currentCalculatorScreenText.length - 1);
+}
+
+function evaluate(operations){
+    const equation = operations.split(" ");
+    const a = Number(equation[0]);
+    const operation = equation[1];
+    const b = Number(equation[2]);
+    console.log(a,b, operation);
+    switch (operation){
+        case "+":
+            return add(a,b)
+        case "-":
+            return subtract(a,b)
+        case "x":
+            return multiply(a,b)
+        case "/":
+            return divide(a,b)
+    }
+}
+
+function getCurrentCalculatorScreen(){
+    return calculatorScreen.textContent;
+}
+
+
+function init(){
+    buttons.forEach((button,index) => {
+    button.addEventListener("click", event =>{
+        const eventTextContent = event.target.textContent.trim();
+        const currentText = getCurrentCalculatorScreen();
+
+        if (eventTextContent === "delete"){
+            const resultScreen = deletePrevCalculatorScreen(currentText);
+        }else if(eventTextContent === "clear"){
+            calculatorScreen.textContent = "";
+        }else if(eventTextContent === "="){
+            const currentValueCalculatorScreen = getCurrentCalculatorScreen();
+            const result = evaluate(currentValueCalculatorScreen);
+            console.log("result of evaluation: ", result)
+            setCalculatorScreen(result);
+        }else{
+            updateCalculatorScreen(eventTextContent);
+        }
+    })
+    })
+}
+init();
+
+// console.log(calculatorScreen.textContent);
+
 //todo implementation first of add,delete,multiply,divide with error handling and such.
 //add event listeners to button to manipulate calculator screen content.
 //make clear button clear everything from the calculator screen content
